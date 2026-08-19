ce8fe15470030bb0285e1c40104d2dd758900475
victorchiappetta <victorcchiappetta@gmail.com>
2026-08-19 16:26:30 -0300

    feat: analisa se existe divergencia de carga diária (tabela carga_consolidada) VS carga consolidada(balanco_consolidado); utiliza CTE, INNER JOIN

---
+++ queries_teste/queries_cruzadas/02.sql
@@ -0,0 +1,61 @@
+
+/*
+    Para cada subsistema e dia, compare
+    a carga registrada em carga_diaria
+    com a soma das leituras de carga
+    em balanco_consolidado.
+
+    classifique como "divergente" qualquer
+    dia que a diferença absoluta
+    ultrapasse 5% do valor de carga_diaria
+
+
+*/
+
+WITH cargaBalancoPorDia AS (
+    SELECT
+            id_subsistema,
+            strftime('%Y-%m-%d', din_instante) AS dia,
+            avg(val_carga) AS mediaCargaBalanco
+                
+    FROM balanco_consolidado
+    GROUP BY id_subsistema, dia
+),
+
+tb_join AS (
+    SELECT
+        t1.dia AS dia,
+        t1.id_subsistema,
+        mediaCargaBalanco,
+        val_cargaenergiamwmed
+    FROM cargaBalancoPorDia AS t1
+
+    INNER JOIN carga_consolidada AS t2
+    ON t1.id_subsistema = t2.id_subsistema
+    AND t1.dia = t2.din_instante
+),
+
+tb_diferencas AS (
+
+    SELECT
+            dia,
+            id_subsistema,
+            abs((mediaCargaBalanco - val_cargaenergiamwmed) *100.0 / val_cargaenergiamwmed) AS difAbsoluta,
+            CASE
+                WHEN abs((mediaCargaBalanco - val_cargaenergiamwmed) *100.0 / val_cargaenergiamwmed) > 5 THEN 'Divergente'
+                ELSE 'Normal'
+            END AS classificacao
+    FROM tb_join
+
+)
+
+
+SELECT 
+        id_subsistema,
+        dia,
+        round(difAbsoluta, 13) AS "difAbsoluta(%)",
+        classificacao
+
+FROM tb_diferencas
+
+ORDER BY difAbsoluta DESC
